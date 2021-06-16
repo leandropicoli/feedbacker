5fbfcb089d4ee21b3f7c9a2c1f3c82debff4b63b
leandro <leandropicoli@live.com>
2021-06-16 20:19:27 -0300

    Starting modal factory

---
+++ src/components/ModalFactory/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <teleport to="body">
+      <div
+        v-if="state.isActive"
+        class="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50"
+        @click="handleModalToogle({ status: false })"
+      >
+          <div class="fixed mx-10">
+              <div class="flex flex-col overflow-hidden bg-white rounded-lg animate__animated animate__fadeInDown animate__faster">
+                  <div class="flex flex-col px-12 py-10 bg-white">
+                      <component :is="state.component"></component>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </teleport>
+</template>
+
+<script>
+import { reactive } from 'vue'
+
+const DEFAULT_WIDTH = 'w-3/4 lg:w-1/3'
+
+export default {
+  setup () {
+    const state = reactive({
+      isActive: false,
+      component: {},
+      props: {},
+      width: DEFAULT_WIDTH
+    })
+
+    function handleModalToogle ({ status }) {
+
+    }
+
+    return {
+      state
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
